8c8e55f8bc07996793f1f90ab0a30f082f370b3f
lumwesley <lum.wesley@gmail.com>
2018-03-28 21:45:46 -0700

    Added test file

---
+++ test.py
@@ -0,0 +1,7 @@
+def multiply(x, y):
+    return x*y
+
+first = int(input("Please enter the first number: "))
+second = int(input("Please enter the second number: "))
+
+print(multiply(first,second))
